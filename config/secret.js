b9229630b94e065576de57f517f8262a8848121e
uzzol101 <davepillowgood@gmail.com>
2017-09-25 10:42:53 +0600

    modified dbpath

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: "mongodb://uzzol101:m08520280@ds137054.mlab.com:37054/amazon",
+    database: "mongodb://username:password@ds137054.mlab.com:37054/amazon",
     secretKey: "uzzol@#{}!&^0#",
     port: process.env.PORT || 3000
 }
