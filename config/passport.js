63fc6aa91c82caad31864fa55ef1394d9a1616fe
uzzol101 <davepillowgood@gmail.com>
2017-09-25 10:36:08 +0600

    improved flash messaging

--- config/passport.js
+++ config/passport.js
@@ -38,10 +38,11 @@ passport.use("local-login", new LocalStrategy({
         user.comparePassword(password).then((isMatch) => {
 
             if (isMatch) {
-                req.flash("success","Welcome back");
+
+                req.flash("success", "Welcome back");
                 return done(null, user);
             } else {
-                req.flash("error","Oops wrong password");
+                req.flash("error", "Oops wrong password");
                 return done(null, false)
             }
         });
